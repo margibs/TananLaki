98c03111c634494194cda6e1cb7307317be1b520
Rumar Gibs Ecaldre <rumargibs@gmail.com>
2015-10-06 15:53:48 +0800

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,8 +43,6 @@ class HomeController extends Controller {
 
         $this->data['categories'] = Categories::where('id','!=',6)->where('id','!=',4)->where('id','!=',2)->get();
 
-        // $this->data['side_bar_posts'] = Posts::where('status',1)->orderBy('id','DESC')->take(5)->get();
-
         $this->data['side_bar_posts'] = DB::table('posts')
             ->join('post_categories','post_categories.post_id','=','posts.id')
             ->join('categories','post_categories.category_id','=','categories.id')
@@ -55,11 +53,6 @@ class HomeController extends Controller {
             ->take(10)
             ->get();
         
-        // foreach ($this->data['side_bar_posts'] as $post)
-        // {
-        //     $post->categories2 = $this->customQuery->getPostCategories($post->id,false);
-        // }
-        // dd($this->data['side_bar_posts']);
         $customQuery->per_page = 16;
     }
 
